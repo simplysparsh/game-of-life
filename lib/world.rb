685d959c2cccb4d7d65ace86939814681d69ab50
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-06-15 21:10:56 +0530

    Update logic for changing cell status and complete its specs

--- lib/world.rb
+++ lib/world.rb
@@ -12,11 +12,21 @@ class World
     live_neighbours = get_live_neighbours(cell)
 
     if cell.is_alive?
-      if live_neighbours.count < 2
+      if live_neighbours.count < 2 || live_neighbours.count > 3
+        cell.switch_status
+      end
+    else
+      if live_neighbours.count == 3
         cell.switch_status
       end
     end
 
+  end
+
+  def set_initial_live_cells(position_list)
+    position_list.each do |position|
+      @grid[position].switch_status
+    end
   end
 
   private
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,19 +5,41 @@ describe 'World'  do
 
   describe '#set_cell_status' do
 
-    context 'when the cell is alive' do
-      let(:cell) { Cell.new(:alive) }
+    let(:world) { World.new }
 
-      it 'dies if it has less than two live neighbours' do
-        expect(cell.get_status).to eq(:dead)
+    context 'when the cell is alive' do
+
+      it 'sets status to dead if cell has less than two live neighbours' do
+        world.set_initial_live_cells([10,9])
+        world.set_cell_status(world.grid[10])
+
+        expect(world.grid[10].status).to eq(:dead)
       end
 
-      it 'dies if it has more than three live neighbours'
-      it 'continues to live if it has two or three live neighbours'
+      it 'sets status to dead if cell has more than three live neighbours' do
+        world.set_initial_live_cells([10,9,11,0,20])
+        world.set_cell_status(world.grid[10])
+
+        expect(world.grid[10].status).to eq(:dead)
+      end
+
+      it 'continues to live if it has two or three live neighbours' do
+        world.set_initial_live_cells([10,9,11])
+        world.set_cell_status(world.grid[10])
+
+        expect(world.grid[10].status).to eq(:alive)
+      end
     end
 
     context 'when the cell is dead' do
-      it 'comes to life if it has exactly three neighbours'
+
+      it 'comes to life if it has exactly three live neighbours'do
+        world.set_initial_live_cells([9,11,0])
+        world.set_cell_status(world.grid[10])
+
+        expect(world.grid[10].status).to eq(:alive)
+
+      end
     end
 
   end
