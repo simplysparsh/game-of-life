7a96633584ca21cb518495fb6de9c9198cf3718c
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-06-30 15:07:41 -0400

    Refactor to accomodate the new Grid class

--- lib/world.rb
+++ lib/world.rb
@@ -1,15 +1,16 @@
 require_relative "cell.rb"
+require_relative "grid.rb"
 
 class World
 
   attr_reader :grid
 
   def initialize
-    @grid = create_cells # 10 x 10 grid
+    @grid = Grid.new
   end
 
   def set_cell_status(cell)
-    live_neighbours = get_live_neighbours(cell)
+    live_neighbours = grid.get_live_neighbours(cell)
 
     if cell.is_alive?
       handle_live_cell(cell, live_neighbours)
@@ -18,42 +19,7 @@ class World
     end
   end
 
-  def set_initial_live_cells(position_list)
-    position_list.each do |position|
-      @grid[position].switch_status
-    end
-  end
-
   private
-
-  def create_cells
-    cells = []
-    position = 0
-    100.times do
-      cells << Cell.new(position)
-      position = position + 1
-    end
-    cells
-  end
-
-  def get_live_neighbours(cell)
-    live_neighbours = []
-    neighbour_positions = get_neighbour_positions(cell)
-
-    neighbour_positions.each do |neighbour_position|
-      neighbour_cell = @grid[neighbour_position]
-      live_neighbours << neighbour_cell if neighbour_cell.is_alive?
-    end
-
-    live_neighbours
-  end
-
-  def get_neighbour_positions(cell)
-    [(cell.position - 10), (cell.position + 10), # vertical
-     (cell.position -  1), (cell.position +  1), # horizontal
-     (cell.position - 11), (cell.position + 11), # left diagonal
-     (cell.position -  9), (cell.position +  9)] # right diagonal
-  end
 
   def handle_live_cell(cell, live_neighbours)
     if rule_one_applies?(live_neighbours) || rule_two_applies?(live_neighbours)
@@ -78,6 +44,6 @@ class World
   def rule_three_applies?(live_neighbours)
     live_neighbours.count == 3
   end
-  
+
 end
 
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -6,42 +6,32 @@ describe 'World'  do
   let(:world) { World.new }
   let(:grid) { world.grid }
 
-  describe '#set_initial_live_cells' do
-
-    it 'sets the status of the cells, on the given positions, to alive' do
-      cell_positions = [94]
-      world.set_initial_live_cells(cell_positions)
-
-      expect(grid[94].status).to eq(:alive)
-    end
-  end
-
   describe '#set_cell_status' do
 
     context 'when the cell is alive' do
 
       it 'sets status to dead if cell has less than two live neighbours' do
         cell_positions = [10, 9]
-        world.set_initial_live_cells(cell_positions)
+        grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid[10])
-        expect(grid[10].status).to eq(:dead)
+        world.set_cell_status(grid.cells[10])
+        expect(grid.cells[10].status).to eq(:dead)
       end
 
       it 'sets status to dead if cell has more than three live neighbours' do
         cell_positions = [10,9,11,0,20]
-        world.set_initial_live_cells(cell_positions)
+        grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid[10])
-        expect(grid[10].status).to eq(:dead)
+        world.set_cell_status(grid.cells[10])
+        expect(grid.cells[10].status).to eq(:dead)
       end
 
       it 'lets the status to be alive if it has two or three live neighbours' do
         cell_positions = [10,9,11,0]
-        world.set_initial_live_cells(cell_positions)
+        grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid[10])
-        expect(grid[10].status).to eq(:alive)
+        world.set_cell_status(grid.cells[10])
+        expect(grid.cells[10].status).to eq(:alive)
       end
     end
 
@@ -49,10 +39,10 @@ describe 'World'  do
 
       it 'sets status to alive if it has exactly three live neighbours'do
         cell_positions = [9,11,0]
-        world.set_initial_live_cells(cell_positions)
+        grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid[10])
-        expect(grid[10].status).to eq(:alive)
+        world.set_cell_status(grid.cells[10])
+        expect(grid.cells[10].status).to eq(:alive)
 
       end
     end
