26ea98236255a83e2fc6542f534c0d346f4e6351
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-07-06 14:33:05 -0400

    Use `update_world` method to get the new state of the world

--- lib/world.rb
+++ lib/world.rb
@@ -9,40 +9,67 @@ class World
     @grid = Grid.new
   end
 
-  def set_cell_status(cell)
-    live_neighbours = grid.get_live_neighbours(cell)
-
-    if cell.is_alive?
-      handle_live_cell(cell, live_neighbours)
-    else
-      handle_dead_cell(cell, live_neighbours)
-    end
+  def update_world
+    cells_to_update = get_cells_to_update
+    kill_cells(cells_to_update[:cells_to_kill])
+    revive_cells(cells_to_update[:cells_to_revive])
   end
 
   private
 
-  def handle_live_cell(cell, live_neighbours)
-    if rule_one_applies?(live_neighbours) || rule_two_applies?(live_neighbours)
+  def get_cells_to_update
+    cells_to_update = {cells_to_kill:[], cells_to_revive:[]}
+
+    @grid.cells.each do |cell|
+
+      if cell_should_die?(cell)
+        cells_to_update[:cells_to_kill] << cell
+      elsif cell_should_live?(cell)
+        cells_to_update[:cells_to_revive] << cell
+      end
+
+    end
+    cells_to_update
+  end
+
+  def cell_should_die?(cell)
+    rule_one_applies?(cell) || rule_two_applies?(cell)
+  end
+
+  def cell_should_live?(cell)
+    rule_three_applies?(cell) || rule_four_applies?(cell)
+  end
+
+  def kill_cells(cells)
+    cells.each do |cell|
       cell.kill
     end
   end
 
-  def handle_dead_cell(cell, live_neighbours)
-    if rule_three_applies?(live_neighbours)
+  def revive_cells(cells)
+    cells.each do |cell|
       cell.revive
     end
   end
 
-  def rule_one_applies?(live_neighbours)
-    live_neighbours.count < 2
+  def rule_one_applies?(cell)
+    live_neighbours = @grid.get_live_neighbours(cell)
+    live_neighbours.count < 2 && cell.is_alive?
   end
 
-  def rule_two_applies?(live_neighbours)
-    live_neighbours.count > 3
+  def rule_two_applies?(cell)
+    live_neighbours = @grid.get_live_neighbours(cell)
+    live_neighbours.count > 3 && cell.is_alive?
   end
 
-  def rule_three_applies?(live_neighbours)
-    live_neighbours.count == 3
+  def rule_three_applies?(cell)
+    live_neighbours = @grid.get_live_neighbours(cell)
+    live_neighbours.count == 3 && cell.is_dead?
+  end
+
+  def rule_four_applies?(cell)
+    live_neighbours = @grid.get_live_neighbours(cell)
+    cell.is_alive? && ([2, 3].include? live_neighbours)
   end
 
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -6,7 +6,7 @@ describe 'World'  do
   let(:world) { World.new }
   let(:grid) { world.grid }
 
-  describe '#set_cell_status' do
+  describe '#update_world' do
 
     context 'when the cell is alive' do
 
@@ -14,7 +14,7 @@ describe 'World'  do
         cell_positions = [10, 9]
         grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid.cells[10])
+        world.update_world
         expect(grid.cells[10].status).to eq(:dead)
       end
 
@@ -22,26 +22,26 @@ describe 'World'  do
         cell_positions = [10,9,11,0,20]
         grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid.cells[10])
+        world.update_world
         expect(grid.cells[10].status).to eq(:dead)
       end
 
       it 'lets the status to be alive if it has two or three live neighbours' do
-        cell_positions = [10,9,11,0]
+        cell_positions = [54, 43, 44, 65]
         grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid.cells[10])
-        expect(grid.cells[10].status).to eq(:alive)
+        world.update_world
+        expect(grid.cells[54].status).to eq(:alive)
       end
     end
 
     context 'when the cell is dead' do
 
       it 'sets status to alive if it has exactly three live neighbours'do
-        cell_positions = [9,11,0]
+        cell_positions = [20,11,0]
         grid.set_initial_live_cells(cell_positions)
 
-        world.set_cell_status(grid.cells[10])
+        world.update_world
         expect(grid.cells[10].status).to eq(:alive)
 
       end
