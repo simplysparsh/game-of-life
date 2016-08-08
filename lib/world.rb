3cf62a40a45141e1b79da52f9af6eeb93654dd13
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-08-07 21:42:04 -0400

    Intermediate code state
    This commit consists of a broken code that is moving in the right
    direction. I am commiting it as a safety measure since my computer seems
    weird.

--- lib/world.rb
+++ lib/world.rb
@@ -9,26 +9,46 @@ class World
     @grid = Grid.new(grid_size)
   end
 
+  def set_initial_live_cells(cells)
+    @grid.set_initial_live_cells(cells)
+  end
+
   def update_world
     cells_to_update = get_cells_to_update
     kill_cells(cells_to_update[:to_kill])
     revive_cells(cells_to_update[:to_revive])
   end
 
+   def is_equal?(world)
+     @grid.is_equal?(world.grid)
+   end
+
   private
 
   def get_cells_to_update
     cells_to_update = {to_kill:[], to_revive:[]}
 
-    @grid.cells.each do |row_cells|
-      row_cells.each do |cell|
-        if cell_should_die?(cell)
-          cells_to_update[:to_kill] << cell
-        elsif cell_should_live?(cell)
-          cells_to_update[:to_revive] << cell
-        end
+    # Set of cells
+    # Add live cells to set of cells
+    # Add neighbours of live cells to set of cells
+
+    @grid.get_cells.each do |cell_position|
+      cell = @grid.get_cell(cell_position)
+      if cell_should_die?(cell)
+        cells_to_update[:to_kill] << cell
+      elsif cell_should_live?(cell)
+        cells_to_update[:to_revive] << cell
       end
     end
+    # @grid.live_cells.each do |row_cells|
+    #   row_cells.each do |cell|
+    #     if cell_should_die?(cell)
+    #       cells_to_update[:to_kill] << cell
+    #     elsif cell_should_live?(cell)
+    #       cells_to_update[:to_revive] << cell
+    #     end
+    #   end
+    # end
     cells_to_update
   end
 
@@ -43,18 +63,20 @@ class World
   def kill_cells(cells)
     cells.each do |cell|
       cell.kill
+      @grid.update({x: cell.x, y: cell.y}, :kill)
     end
   end
 
   def revive_cells(cells)
     cells.each do |cell|
       cell.revive
+      @grid.update({x: cell.x, y: cell.y}, :create)
     end
   end
 
   def rule_one_applies?(cell)
     live_neighbours = @grid.get_live_neighbours(cell)
-    live_neighbours.count < 2 && cell.is_alive?
+    live_neighbours.count < 2 && cell.is_alive? #@grid.live_cell?({x: cell.x, y: cell.y})
   end
 
   def rule_two_applies?(cell)
@@ -71,6 +93,9 @@ class World
     live_neighbours = @grid.get_live_neighbours(cell)
     cell.is_alive? && ([2, 3].include? live_neighbours)
   end
+
+
+
 
 end
 
