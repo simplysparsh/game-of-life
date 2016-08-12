17380f0cc7f68dd28eb2aeec95ee5a6272da5d83
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-08-11 21:54:39 -0400

    Update tests as per design changes

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -9,67 +9,77 @@ describe 'World'  do
   describe '#update_world' do
 
     context 'when the cell is alive' do
+      let(:updated_world) {World.new(10)}
 
       it 'sets status to dead if cell has less than two live neighbours' do
-        cells_to_revive = [grid.cell(0,1), grid.cell(9,0)]
-        grid.set_initial_live_cells(cells_to_revive)
+        world.set_initial_live_cells([{x: 0, y: 1}, {x: 9, y: 0}])
 
         world.update_world
-        expect(grid.cell(0,1).status).to eq(:dead)
+
+        expect(world.is_equal?(updated_world)).to be(true)
+        expect(grid.cells.length).to be 0
       end
 
       it 'sets status to dead if cell has more than three live neighbours' do
-        cells_to_revive = [grid.cell(0,1), grid.cell(1,1),
-                           grid.cell(1,0), grid.cell(0,0), grid.cell(0,2)]
-
-        grid.set_initial_live_cells(cells_to_revive)
+        world.set_initial_live_cells(
+            [{x: 0, y: 1}, {x: 1, y: 1},
+             {x: 1, y: 0}, {x: 0, y: 0}, {x: 0, y: 2}])
+        updated_world.set_initial_live_cells(
+            [{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y:2}])
 
         world.update_world
-        expect(grid.cell(0,1).status).to eq(:dead)
+
+        expect(world.is_equal?(updated_world)).to be(true)
+        expect(grid.cells.length).to be 3
       end
 
       it 'lets the status to be alive if it has two or three live neighbours' do
-        cells_to_revive = [grid.cell(4,5), grid.cell(3,4),
-                           grid.cell(4,4), grid.cell(5,6)]
 
-        grid.set_initial_live_cells(cells_to_revive)
+        world.set_initial_live_cells(
+            [{x: 4, y: 5}, {x: 3, y: 4}, {x: 4, y: 4}, {x: 5, y: 6}])
+        updated_world.set_initial_live_cells(
+            [{x: 4, y: 4}, {x: 4, y: 5}, {x: 3, y: 4}])
 
         world.update_world
-        expect(grid.cell(4,5).status).to eq(:alive)
+
+        expect(world.is_equal?(updated_world)).to be(true)
+        expect(grid.cells.length).to be 3
       end
     end
 
 
     context 'when the cell is dead' do
+      let(:updated_world) {World.new(10)}
 
       it 'sets status to alive if it has exactly three live neighbours'do
-        cells_to_revive = [grid.cell(0,2), grid.cell(1,1), grid.cell(0,0)]
-        grid.set_initial_live_cells(cells_to_revive)
+        world.set_initial_live_cells([{x: 0, y: 2}, {x: 1, y: 1}, {x: 0, y: 0}])
+        updated_world.set_initial_live_cells([{x: 0, y: 1}, {x: 1, y: 1}])
 
         world.update_world
-        expect(grid.cell(0,1).status).to eq(:alive)
+
+        expect(world.is_equal?(updated_world)).to be(true)
+        expect(grid.cells.length).to be 2
       end
     end
 
+
     context 'when a world has a particular cell distribution on the grid' do
       let(:updated_world) {World.new(10)}
-      let(:updated_grid) { updated_world.grid }
 
       it 'updates the world to a newer cell distribution as per the game rules' do
+
         world.set_initial_live_cells(
-            [grid.cell(1,2), grid.cell(2,2), grid.cell(3,2),
-             grid.cell(0,9), grid.cell(1,8), grid.cell(2,7),
-             grid.cell(9,9), grid.cell(8,9), grid.cell(8,8), grid.cell(9,8)])
+            [{x: 1, y: 2},{x: 2, y: 2},{x: 3, y: 2},
+             {x: 0, y: 9},{x: 1, y: 8},{x: 2, y: 7},
+             {x: 9, y: 9},{x: 8, y: 9},{x: 8, y: 8},{x: 9, y: 8}])
         updated_world.set_initial_live_cells(
-            [updated_grid.cell(1,8),
-             updated_grid.cell(2,2), updated_grid.cell(2,1), updated_grid.cell(2,3),
-             updated_grid.cell(9,9), updated_grid.cell(8,9), updated_grid.cell(8,8), updated_grid.cell(9,8)])
+            [{x: 1, y: 8}, {x: 2, y: 2},{x: 2, y: 1},{x: 2, y: 3},
+             {x: 9, y: 9},{x: 8, y: 9},{x: 8, y: 8},{x: 9, y: 8}])
 
         world.update_world
         expect(world.is_equal?(updated_world)).to be(true)
       end
-
     end
-  end
 
+  end
 end
